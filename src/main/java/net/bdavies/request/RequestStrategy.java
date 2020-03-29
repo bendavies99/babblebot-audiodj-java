12be3290b77e0ea2606f6234fdd08f8b0a382163
Ben Davies <ben@bj-davies.co.uk>
2020-03-30 00:10:09 +0100

    feat(bot): Added all essential commands

---
+++ src/main/java/net/bdavies/request/RequestStrategy.java
@@ -0,0 +1,21 @@
+package net.bdavies.request;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import net.bdavies.music.MusicTrack;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author ben.davies99@outlook.com (Ben Davies)
+ * @since 1.0.0
+ */
+public interface RequestStrategy {
+    /**
+     * This will be ran when a new request is made.
+     *
+     * @param request        - This is the request that has been made by the user.
+     * @param commandContext - This is the message object that came from the command context.
+     * @param musicBot       - The musicBot plugin instance.
+     * @return AudioTrack - audiotrack enQueue.
+     */
+    Mono<MusicTrack> handle(String request, AudioPlayerManager manager);
+}
